5b7ab2e1a8248283653a18a5bdae986d7a8ee66a
paul <paul.thiongo@student.moringaschool.com>
2023-12-21 16:25:30 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(running){
+    let activity = running || 'roller-skate';
+    return ('This Saturday, I want to '+activity+'!')
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+let wrapAdjective = function(sign='||'){
+    return function(adjective='a programmer'){
+        return `You are ${sign}${adjective}${sign}!`;
+}
+}
